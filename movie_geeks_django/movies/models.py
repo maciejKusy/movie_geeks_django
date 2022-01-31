16ce83d2d529991c1ac95adddb273e50c098eb34
MKurtz666 <mkusy936@gmail.com>
2022-01-31 21:47:11 +0100

    Made further modifications to models. To do: Award types need to be specified explicitly as choices in a separate file so that Awards Received can be created independently of records existing in the Awards table

--- movie_geeks_django/movies/models.py
+++ movie_geeks_django/movies/models.py
@@ -1,18 +1,20 @@
+from django.contrib.auth import forms
 from django.db import models
 from datetime import datetime
 
 
 class Film(models.Model):
-
-    YEAR_CHOICES = list()
-    for year in range(1888, datetime.now().year + 1):
-        YEAR_CHOICES.append((year, year))
-
     name = models.CharField(max_length=50, blank=False)
-    year_of_release = models.DateField(choices=YEAR_CHOICES)
+    year_of_release = models.IntegerField(blank=False)
     synopsis = models.TextField(max_length=200, blank=False)
     director = models.ForeignKey('performers.Performer', on_delete=models.PROTECT, blank=True, null=True)
     cast = models.ManyToManyField('performers.Performer', related_name='films', blank=True)
 
     def __str__(self):
         return f'{self.name} ({self.year_of_release})'
+
+    def clean(self):
+        if self.year_of_release < 1888 or self.year_of_release > datetime.now().year:
+            raise forms.ValidationError('Invalid year provided!')
+
+
